a1326900a04f99dc4686da54cac8dbcb2a792096
Pitter <PITTER775@GAMIL.COM>
2022-08-10 10:49:14 -0300

    .

--- resources/views/pages/usuario/impressaoProntuario.blade.php
+++ resources/views/pages/usuario/impressaoProntuario.blade.php
@@ -1,19 +1,28 @@
-
 <style>
     body {width: 100%;height: 100%;margin: 0;padding: 0;font: 12pt "Tahoma";}
     * {box-sizing: border-box;-moz-box-sizing: border-box;}
     .page {width: 210mm;min-height: 297mm;padding: 10mm;margin: 5mm auto;border-radius: 5px;background: white;box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);}
-    .subpage {padding: 0cm;/* border: 5px red solid; */height: 277mm;/* outline: 1cm #FFEAEA solid; */}
-    .imgescola{ width: 80px; float: right; margin-left: 20PX}
+    .subpage {padding: 0cm;/* border: 5px red solid; /height: 277mm;/ outline: 1cm #FFEAEA solid; */}
+    .imgescola{ width: 80px; float: right; margin-left: 20px; margin-top: -10px;}
     .row{display: table; width: 100%}
     .col-left{float: left;}
     .col-right{float: right;}
     .aling-right{ text-align: right}
     .font-pequena { font-size: 12px}
-    .prontuario{ font-weight: 700;}
+    .prontuario{  font-size: 25px; margin-top: 0px; }
+    .headescola{ font-size: 10px; text-align: right; float: right;}
+    .headescola p{line-height: 15px;}
+    .imgaluno{ margin-top: -5px; width: 130px; height: 130px; background-image: url("https://cangurunews.com.br/wp-content/uploads/2021/12/soluco-em-criancas-696x464.jpg");
+        background-size: cover; background-position: center;        float: left; margin-right: 20px;        border-radius: 5px;
+        box-shadow: 0 4px 8px 0 rgb(34 41 47 / 12%), 0 2px 4px 0 rgb(34 41 47 / 8%);  }
+        
+    .blocofoto2{ font-size: 12px; margin-top: 40px; line-height: 20px;}
+    .blocofoto{ font-size: 12px; display: table; width: 100%;}
+    .blocofoto p{ line-height: 20px;}
+    .blocofoto2 p{ line-height: 20px;}
     
-    @page {size: A4; margin: 0;}
-    @media print {html, body {width: 210mm; height: 297mm;}
+    @page  {size: A4; margin: 0;}
+    @media  print {html, body {width: 210mm; height: 297mm;}
         .page {margin: 0;border: initial;border-radius: initial;width: initial;min-height: initial;box-shadow: initial;background: initial;page-break-after: always;}
     }
 </style>
@@ -21,24 +30,48 @@
     <div class="book">
         <div class="page">
             <div class="subpage">
-                <head>
-                    <div class="row">
+                <head >
+                    <div class="row" style="margin-bottom:40px">
                         <div class="col-left">
                             <p class="prontuario">PRONTUÁRIO</p>
                         </div>
-                        <div class="col-right aling-right font-pequena" style="width: 400px; ">
-                            <img src="{{asset('app-assets/images/escola-drummond.jpg')}}" class="imgescola" />
-                            <p>Carlos Drummond de Andrade</p>
-                            <p>Chácara Solar Jaguari Santana do Parnaíba</p>
+                        <div class="headescola" style="width: 400px; margin-top: -10px;">
+                            <img src="https://educacaofuturo.org.br/app-assets/images/escola-drummond.jpg" class="imgescola" />
+                            <p><b>Carlos Drummond de Andrade</b> <br>
+                            Rua Órbita 182 Chácara Solar Jaguari Santana do Parnaíba <br>
+                            secretaria@educacaofuturo.org.br - 11 4257-2198</p>
                         </div>
                     </div>
                 </head>
-                <div>
-                    <p> Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba </p>
+                <div class="blocofoto" >
+                    <div class="imgaluno"></div>   
+                    <p style="font-size: 16; margin-bottom: 25px;"><b>Abraão Leite Queiroz Castro Filho</b></p>      
+                    <p>
+                        <b>Data de nascimento:</b> 11/07/2019 <span style=" margin: 0 10px;">-</span>
+                        <b>RG:</b> 34570946-7  <span style=" margin: 0 10px;">-</span>
+                        <b>CPF:</b> 30519902807  <br/>
+                        <b>Reg. nascimento:</b> Vitoria da Conquista - SP  <span style=" margin: 0 10px;">-</span>
+                        <b>Sexo:</b> Masculino  <br><br>
+                        <b>Tipo:</b> Matrículado  <span style=" margin: 0 10px;">-</span>
+                        <b>Professora:</b> Tamires  <span style=" margin: 0 10px;">-</span>
+                        <b>Serie:</b> Maternal I - Integral - Bondade  
+                    </p>           
                 </div>
-                <div style="margin-top: 800px">
-                    <p> Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba </p>
-                    <p> Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba Chácara Solar Jaguari Santana do Parnaíba </p>
+                <div class="blocofoto2" >
+                    <p style="font-size: 16; margin-bottom: 10px;"><b>Endereço</b></p> 
+                    <b>Rua:</b> Antonio de Oliveira, 27 <span style=" margin: 0 10px;">-</span>
+                    <b>Complemento:</b> casa 8 <span style=" margin: 0 10px;">-</span>
+                    <b>CEP:</b> 06533-210 <br>
+                    <b>Bairro:</b> Pq Jaguari <span style=" margin: 0 10px;">-</span>
+                    <b>Cidade:</b> Santana de Parnaíba <span style=" margin: 0 10px;">-</span>
+                    <b>Estado:</b> São Paulo 
+                </div>
+                <div class="blocofoto2" >
+                    <p style="font-size: 16; margin-bottom: 10px;"><b>Responsáveis</b></p> 
+                    <b>Nome do Pai:</b> Abraão Leite Queiroz Castro <span style=" margin: 0 10px;">-</span>
+                    <b>Telefone:</b> (11) 9 4025-6387 <br>
+                    <b>Nome do Mãe:</b> Joscinelma Oliveira de Castro Leite <span style=" margin: 0 10px;">-</span>
+                    <b>Telefone:</b> (11) 9 7890-3047 
                 </div>
                 
     
@@ -53,5 +86,5 @@
 
 
 <script>
-    window.print();
+    // window.print();
 </script>
