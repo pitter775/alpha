fd7ae80864413db05b8b408ed90ddaedc11e711e
Pitter <PITTER775@GAMIL.COM>
2022-08-31 14:52:15 -0300

    .

--- resources/views/pages/site/teste.blade.php
+++ resources/views/pages/site/teste.blade.php
@@ -1,72 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-  <meta charset="utf-8">
-  <!-- <meta content="width=device-width, initial-scale=1.0" name="viewport"> -->
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-
-  <title>Educação para o Futuro</title>
-
-
-
-    <!-- Palavras chave -->
-    <meta name="keywords" content="Educação, Futuro" />
-    
-
-<!-- Descrição do site -->
-
-    <meta name="description" content="Qualquer esforço pela educação é um sonho de sociedade mais justa."/>
-    <meta name="theme-color" content="#66619c">
-    <meta name="author" content="pitter775@gmail.com">
-
-    <meta property="og:site_name" content="Educação para o Futuro">
-    <meta property="og:title" content="Instituto Educação para o Futuro">
-    <meta property="og:description" content="Qualquer esforço pela educação é um sonho de sociedade mais justa."/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:locale" content="pt_BR"/>    
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="https://educacaofuturo.org.br/">
-    <meta property="og:image:type" content="image/jpeg">
-    <meta property="og:image" content="https://educacaofuturo.org.br/paper/assets/img/logo_preto3.png" />
-    
-  <!-- Favicons -->
-  <link href="{{ asset('paper') }}/assets/img/icon_logo.png" rel="icon">
-  <link href="{{ asset('paper') }}/assets/img/icon_logo.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Nunito:ital,wght@0,300;0,500;1,200;1,900&family=Quicksand:wght@300&display=swap" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('paper') }}/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="{{ asset('paper') }}/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-  <link href="{{ asset('paper') }}/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="{{ asset('paper') }}/assets/vendor/venobox/venobox.css" rel="stylesheet">
-  <link href="{{ asset('paper') }}/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  <link href="{{ asset('paper') }}/assets/vendor/aos/aos.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="{{ asset('paper') }}/assets/css/style.css" rel="stylesheet">
-
+    <meta charset="utf-8">
+    <title>How to Generate QR Code in Laravel 8</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
 </head>
 
 <body>
 
+    <div class="container mt-4">
 
-  <style>
-    a.linkpub{ color: #777; font-family: "Open Sans", sans-serif !important; font-size: 13px; margin-top: -10px}
-    a.linkpub:hover{ color: #fff;}
-    .icon-box2{ padding: 10px !important}
-    h3 {   font-family: "Nunito"; font-weight: 500;}
-  </style>
+        <div class="card">
+            <div class="card-header">
+                <h2>Simple QR Code</h2>
+            </div>
+            <div class="card-body">
+                {!! QrCode::size(300)->generate('https://techvblogs.com/blog/generate-qr-code-laravel-8') !!}
+            </div>
+        </div>
 
-<a href="sms:11987108829?body=A Aluna Julia Rodrigues acabou de chegar">com frase!</a>
+        <div class="card">
+            <div class="card-header">
+                <h2>Color QR Code</h2>
+            </div>
+            <div class="card-body">
+                {!! QrCode::size(300)->backgroundColor(255,90,0)->generate('https://techvblogs.com/blog/generate-qr-code-laravel-8') !!}
+            </div>
+        </div>
 
-<script>
-  window.location.href = "sms:11987108829?body=A Aluna Julia Rodrigues acabou de chegar";
-</script>
+    </div>
 </body>
 </html>
