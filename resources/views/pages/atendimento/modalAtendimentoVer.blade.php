6a0e819ceadfdf8c03f4e07769da06a8a0348ca6
Pitter Rocha Bico <pitter.bico@doc88.com.br>
2022-09-27 17:31:28 -0300

    .

--- resources/views/pages/atendimento/modalAtendimentoVer.blade.php
+++ resources/views/pages/atendimento/modalAtendimentoVer.blade.php
@@ -28,20 +28,19 @@
                 <textarea class="form-control" id="com_texto" name="com_texto" rows="2" placeholder="Escreva o comentário..."></textarea>
                 <input type="hidden" value="{{$ver->id ?? ''}}" name="com_ate_id" id="com_ate_id">
             </div>
-            <div class="chat chat-left">
-                                            <div class="chat-avatar">
-                                                <span class="avatar box-shadow-1 cursor-pointer">
-                  <img src="../../../app-assets/images/portrait/small/avatar-s-20.jpg" alt="avatar" height="36" width="36">
-                </span>
-                                            </div>
-                                            <div class="chat-body">
-                                                <div class="chat-content">
-                                                    <p>Hey John, I am looking for the best admin template.</p>
-                                                    <p>Could you please help me to find it out? 🤔</p>
-                                                </div>
-                                                
-                                            </div>
-                                        </div>
+            <div class="chat">
+                <div class="chat-avatar">
+                    <span class="avatar box-shadow-1 cursor-pointer">
+                        <img src="../../../app-assets/images/portrait/small/avatar-s-20.jpg" alt="avatar" height="36" width="36">
+                    </span>
+                </div>
+                <div class="chat-body">
+                    <div class="chat-content">
+                        <p>Hey John, I am looking for the best admin template.</p>
+                        <p>Could you please help me to find it out? 🤔</p>
+                    </div>                    
+                </div>
+            </div>
         </div>
 
         <div class="col-md-3" style=" padding: 0; margin-right: -20px !important">
